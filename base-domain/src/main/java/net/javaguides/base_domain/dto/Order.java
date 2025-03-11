9d2528358bb24a723c43fa2e5ad458e05e0281a0
Santosh2019 <santoshlimbale76@gmail.com>
2025-03-11 10:21:38 +0530

    added kafka functionality

---
+++ base-domain/src/main/java/net/javaguides/base_domain/dto/Order.java
@@ -0,0 +1,57 @@
+package net.javaguides.base_domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ email-service/src/main/java/net/javaguides/email_service/kafka/OrderConsumer.java
@@ -0,0 +1,17 @@
+package net.javaguides.email_service.kafka;
+
+import net.javaguides.base_domain.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void emailConsumer(OrderEvent event) {
+        logger.info(String.format("Order event received in stock => %s", event));
+    }
+}
